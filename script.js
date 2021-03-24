8a7d29c3b113ed4ccc50a4f0bc1811cfc4efd4b3
Mário André <mario_andre.l.a@hotmail.com>
2021-03-24 16:12:50 -0300

    defined script.js using fetch to request and validate login

---
+++ script.js
@@ -0,0 +1,30 @@
+const init = () => {
+    const inputEmail = document.querySelector('input[type=email]');
+    const inputPassword = document.querySelector('input[type=password]');
+    const submitButton = document.querySelector('.login_submit');
+    
+    // console.log(inputEmail, inputPassword, submitButton);
+
+    if(submitButton){
+        submitButton.addEventListener('click', (event) => {
+            event.preventDefault();
+
+            fetch('https://reqres.in/api/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email: inputEmail.value,
+                    password: inputPassword.value,
+                })
+            }).then((response)=> {
+                return response.json();
+            }).then((data) => {
+                console.log(data)
+            })
+        })
+    }
+}
+
+window.onload = init;
